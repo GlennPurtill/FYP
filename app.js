ae74054bbf92fe8aa7bd7810861a7bbbdab6118f
Glenn Purtill <glennpurtill@Glenns-MacBook-Pro.local>
2019-03-09 13:13:01 +0000

    changes

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ app.get('*', (req, res) => {
 });
 
 // const port = process.env.PORT || '8080';
-// app.set('port', port);
+app.set('port', port);
 
 const server = http.createServer(app);
 
-// server.listen(port, () => console.log(`Running on localhost:${port}`));
+server.listen(port, () => console.log(`Running on localhost:${port}`));

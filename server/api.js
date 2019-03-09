6f53c0019d0bee9a8937a1d8649a2aadb1e67786
Glenn Purtill <glennpurtill@Glenns-MacBook-Pro.local>
2019-03-09 13:33:46 +0000

    changes

--- server/api.js
+++ server/api.js
@@ -26,7 +26,6 @@ api.use(cors())
 api.post('/transSpanish', (req, res, next) => {
     translate(req.query.data, {to: 'es'}).then(response => {
         res.status('200').json(response.text)
-        //=> nl
      }).catch(err => {
         console.error(err);
      });
